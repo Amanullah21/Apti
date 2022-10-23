e0fb1f0a8733b071fe89a16edc7f7b5c92c5b3a1
Amanullah21 <1amanpce@gmail.com>
2022-10-23 22:56:21 +0530

    prime check question

---
+++ math.js
@@ -0,0 +1,23 @@
+// Which one of the following is not a prime number?
+a =	31
+b =	61
+d =	71
+c =	91
+// Answer: Option D
+
+const Prime = (n) => {
+    let Boolean = false;
+    for (let i = 2; i < n; i++) {
+      if (n % i == 0) {
+        Boolean = true;
+      }
+    }
+    Boolean == true
+      ? console.log(n + " is Not Prime")
+      : console.log(n + " is prime");
+  };
+  Prime(a);
+  Prime(b);
+  Prime(c);
+  Prime(d)
+  
